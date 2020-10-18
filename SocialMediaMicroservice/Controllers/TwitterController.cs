5e019b46b516a5351844df4bc90ca79bed058f7c
cshaper <cshaper72@gmail.com>
2020-10-19 00:30:47 +0900

    created empty folder for twitter

---
+++ SocialMediaMicroservice/Controllers/TwitterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaMicroservice.Controllers
+{
+    public class TwitterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
