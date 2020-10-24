f910421dd192f3ecf89a61002144c7b541fb61d1
Dhara Bhandari <dharabhandari1602@gmail.com>
2020-10-24 19:01:53 +0530

    restructured FB code and added user media info API

---
+++ SocialMediaMicroservice/Controllers/InstagramController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaMicroservice.Helper.Instagram;
+using SocialMediaMicroservice.Model;
+
+namespace SocialMediaMicroservice.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InstagramController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddPost([FromBody]InstagramAddPostModel obj)
+        {
+            var result = await new AddPost().Run(obj);
+            return Ok(result ? "success" : "error");
+        }
+    }
+}
